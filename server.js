61960dd0b845dc901557852fc671bf68c7a7bf6c
jeffkealy <jeffkealy@yahoo.com>
2018-10-10 15:46:00 -0500

    set up sever for heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/'));
 
 app.listen('3000');
-console.log('working on 3000');
+console.log('working on', PORT);
